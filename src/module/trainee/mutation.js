89ae590e3eff2fdcb413dc0d9af8ca81de785f03
Shobhit Dwivedi <shobhit.dwivedi@successive.tech>
2021-11-24 12:05:48 +0530

    Subscriptions

--- src/module/trainee/mutation.js
+++ src/module/trainee/mutation.js
@@ -1,16 +1,24 @@
+import pubsub from '../pubsub';
+import constant from '../../lib/constant';
 import userInstance from '../../service/user';
 
 export default {
   createTrainee: (parent, args) => {
     const { user } = args;
-    return userInstance.createUser(user);
+    const addUser = userInstance.createUser(user);
+    pubsub.publish(constant.subscriptions.TRAINEE_ADDED, { traineeAdded: addUser });
+    return addUser;
   },
   updateTrainee: (parent, args) => {
     const { id, role } = args;
-    return userInstance.updateUser(id, role);
+    const updateUser = userInstance.updateUser(id, role);
+    pubsub.publish(constant.subscriptions.TRAINEE_UPDATED, { traineeUpdated: updateUser });
+    return updateUser;
   },
   deleteTrainee: (parent, args) => {
     const { id } = args;
-    return userInstance.deleteUser(id);
+    const deletedId = userInstance.deleteUser(id);
+    pubsub.publish(constant.subscriptions.TRAINEE_DELETED, { traineeDeleted: deletedId });
+    return deletedId;
   },
 };
